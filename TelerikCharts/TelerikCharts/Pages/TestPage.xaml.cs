967cdaf2f9704024ac80b7c19e04d7b57b68f7ca
Matt Seigel <matt.seigel@headspring.com>
2016-07-19 16:55:08 -0500

    added another test page. put the pages in a pages folder.

--- TelerikCharts/TelerikCharts/Pages/TestPage.xaml.cs
+++ TelerikCharts/TelerikCharts/Pages/TestPage.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-
-namespace TelerikCharts
+﻿namespace TelerikCharts.Pages
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xamarin.Forms;
+
     public partial class TestPage : ContentPage
     {
         public TestPage()
---
+++ TelerikCharts/TelerikCharts/Pages/TestPage2.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TelerikCharts.Pages
+{
+    public partial class TestPage2 : ContentPage
+    {
+        public TestPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
